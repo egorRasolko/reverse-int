c8bbc0dee3d78dc306d9a49c38004be1294c7b69
Egor Rasolko <egor.rassolko2018@gmail.com>
2022-01-29 21:19:01 +0300

    add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
+    let num = Math.abs(n);
+    let str = num.toString();
+    let newstr = ' ';
+    for(let i = 0; i < str.length; i++) {
+        newstr = str[i] + newstr;
+    }
+    return Number(newstr)
 }
